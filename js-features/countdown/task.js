12201f28c3739bcc445570cd5828b1d76e0b3bce
Andrey Rodionov <ambal007a@yandex.ru>
2019-05-18 06:47:00 +0400

    Тема1.Задание1.Исправление обнуления счётчика

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -4,13 +4,9 @@ const addTimer = function () {
     timer.textContent -= 1;
     if (Number(timer.textContent) === 0) {
         alert ("Вы победили в конкурсе");
+        clearInterval(id);
     } 
+
 };
 
-
 const id = setInterval(addTimer, 1000);
-
-setTimeout(function() {
-	clearInterval(id);
-}, 60000);
-
