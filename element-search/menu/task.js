19dab75b6d7fc5e09ccab0ccd7f1a5f3d4efafbb
Andrey Rodionov <ambal007a@yandex.ru>
2019-05-22 16:32:37 +0400

    Тема2. Задание2. Второй коммит. Нерабочий

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,18 +1,21 @@
-const menu_link = document.getElementsByClassName('menu__link');
+//const menu_link = document.getElementsByClassName('menu__link');
 
-//const menu_item = document.getElementsByClassName('menu menu_item');
+const menu_item = document.getElementsByClassName('menu_item');
 
-const menu_sub = document.querySelectorAll('.menu_sub');
-console.log(menu_sub);
+let menu_sub = document.querySelectorAll('menu_sub');
+//console.log(menu_sub);
 
-menuLink = Array.from(menu_link);
+menuItem = Array.from(menu_item);
 
 menuSub = Array.from(menu_sub);
 
-for (let i = 0; i < menuLink.length; i++) {
-    menuLink[i].onclick = function() {
-        if (i = 2) {
-            menuSub[i].className = "menu menu_sub menu_active";
+for (let i = 0; i < menuItem.length; i++) {
+    menuItem[i].onclick = function() {
+        console.log('Сработало');
+        menuSub = document.querySelector('menu_sub');
+        console.log(menuSub.closest('menu_item'));
+        if (menuSub.closest('menu_item') == '<li class="menu__item">...</li>') {
+            menuSub.className = "menu menu_sub menu_active";
         }
     }
 }
