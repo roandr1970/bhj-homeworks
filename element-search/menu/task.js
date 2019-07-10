f1ce4c44b83357251fff43a7cbf6454283e57f26
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-10 11:41:55 +0400

    Тема 2. Поиск нужного элемента. Задание 2. Рабочий вариант

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,17 @@
 const menu_item = document.getElementsByClassName('menu__item');
-const elements = document.querySelectorAll('li > ul');
+
 menuItem = Array.from(menu_item);
-//console.log(menuItem);
-Elements = Array.from(elements);
-//console.log(Elements);
+
 for (let i = 0; i < menuItem.length; i++) {
     menuItem[i].onclick = function() {
-        let element = document.querySelector('li > ul');
-        //console.log (element);
-        if (element == 'ul class="menu menu_sub"') {
+        let element = menuItem[i].querySelector('li > ul');
+
+        if (element.className == "menu menu_sub") {
             element.className = "menu menu_sub menu_active";
+        } else if (element.className == "menu menu_sub menu_active") {
+            element.className = "menu menu_sub";
         }
+
+        return false;
     }
 }
