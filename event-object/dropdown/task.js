b82f2eb67749b02f2ff539cce01aaebd4c793129
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-13 08:55:04 +0400

    Тема 3. Объект события. Задание 1. Рабочий вариант

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,22 +1,23 @@
 const dropdown_value = document.getElementsByClassName('dropdown__value');
-const dropdownValue = Array.from(dropdown_value);
+//const dropdownValue = Array.from(dropdown_value);
 const dropdown_list = document.getElementsByClassName('dropdown__list');
-const dropdownList = Array.from(dropdown_list);
+//const dropdownList = Array.from(dropdown_list);
 const dropdown_item = document.getElementsByClassName('dropdown__item');
 const dropdownItem = Array.from(dropdown_item);
+let element;
 
-
-for (let i = 0; i < dropdownValue.length; i++) {
+//for (let i = 0; i < dropdownValue.length; i++) {
     function openList() {
-        dropdownList[i].className = "dropdown__list dropdown__list_active";
+        dropdown_list[0].classList.toggle("dropdown__list_active");
         for (let j = 0; j < dropdownItem.length; j++) {
-            dropdownItem[j].addEventListener('click', function() {       
-                let element = dropdownItem[j].querySelector("ul > li > a");
-                alert(dropdownValue[i].textContent = element.textContent); 
-                dropdownList[i].className = "dropdown__list";
-                return false;
-            })                
+            dropdownItem[j].addEventListener('click', function(event) {
+                event.preventDefault();       
+                element = dropdownItem[j].querySelector(".dropdown__link");
+                dropdown_value[0].textContent = element.textContent; 
+                dropdown_list[0].className = "dropdown__list";
+            })   
+            console.log(j);             
         }
     }
-    dropdownValue[i].addEventListener('click', openList);
-}   
+    dropdown_value[0].addEventListener('click', openList);
+//}   
