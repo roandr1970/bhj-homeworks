873ab3568ae2f6973b1533fea5b91ce3d060f495
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-22 08:21:17 +0400

    Тема5. Работа с HTML-формами. Задание1. Исправление сообщений робота

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -28,10 +28,35 @@ chat_widget_input.addEventListener('change', event => {
 
     chat_widget_input.value = '';
 
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    let a = (getRandomInt(1, 5));
+    let message_robot;
+
+    switch (a) {
+        case 1:
+            message_robot = 'Поздравляю Вас с праздником!';
+            break;
+        case 2:
+            message_robot = 'Давайте знакомиться';
+            break;
+        case 3:
+            message_robot = 'Как Ваши дела?';
+            break;
+        case 4:
+            message_robot = 'У Вас всё в порядке?';
+            break;
+        case 5:
+            message_robot = 'Давайте поболтаем, пока операторы заняты';
+            break;
+    }
+
     messages.innerHTML += `
             <div class="message">
                 <div class="message__time">`+ hours + ":" + minutes + `</div>
-                <div class="message__text"> Я завис, помогите! </div>
+                <div class="message__text">` + message_robot + `</div>
             </div>
         `;
     
