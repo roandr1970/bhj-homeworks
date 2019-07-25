8608c79e2bd9ecbf55f5b392f7fb9ebb9efc5632
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-25 14:38:58 +0400

    Тема5. Работа с HTML-формами. Задание2. Исправление включения дочерних checkbox

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,24 +1,55 @@
 const interest_check = document.querySelectorAll('.interest__check');
 const interestCheck = Array.from(interest_check);
 
+interestCheck[0].addEventListener('change', () => {
+    if (interestCheck[0].checked ) {
+        interestCheck[1].checked = true;
+        interestCheck[2].checked = true;
+    } else {
+        interestCheck[1].checked = false;
+        interestCheck[2].checked = false;
+    }
+})
 
-for (let i = 0; i < interestCheck.length; i++) {
-    interestCheck[i].addEventListener('change', () => {
-    
-        if (interestCheck[0].checked == true) {
-            interestCheck[1].checked = true;
-            interestCheck[2].checked = true;
-        } else {
-            interestCheck[1].checked = false;
-            interestCheck[2].checked = false;
-        }
+interestCheck[1].addEventListener('change', () => {
+    if (interestCheck[1].checked) {
+        interestCheck[0].checked = true;
+    } else if (interestCheck[1].checked == false && interestCheck[2].checked == false) {
+        interestCheck[0].checked = false;
+    }
+})
 
-        if (interestCheck[3].checked == true) {
-            interestCheck[4].checked = true;
-            interestCheck[5].checked = true;
-        } else {
-            interestCheck[4].checked = false;
-            interestCheck[5].checked = false;
-        }
-    })
-}
+interestCheck[2].addEventListener('change', () => {
+    if (interestCheck[2].checked) {
+        interestCheck[0].checked = true;
+    } else if (interestCheck[1].checked == false && interestCheck[2].checked == false) {
+        interestCheck[0].checked = false;
+    }
+})
+
+interestCheck[3].addEventListener('change', () => {
+    if (interestCheck[3].checked ) {
+        interestCheck[4].checked = true;
+        interestCheck[5].checked = true;
+    } else {
+        interestCheck[4].checked = false;
+        interestCheck[5].checked = false;
+    }
+})
+
+interestCheck[4].addEventListener('change', () => {
+    if (interestCheck[4].checked) {
+        interestCheck[3].checked = true;
+    } else if (interestCheck[4].checked == false && interestCheck[5].checked == false) {
+        interestCheck[3].checked = false;
+    }
+})
+
+interestCheck[5].addEventListener('change', () => {
+    if (interestCheck[5].checked) {
+        interestCheck[3].checked = true;
+    } else if (interestCheck[4].checked == false && interestCheck[5].checked == false) {
+        interestCheck[3].checked = false;
+    }
+})
+
