1f2626adecbd6941fe11519c90e5ac46fa44c1a6
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-28 21:01:06 +0400

    Тема6. Подсказки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,12 +1,16 @@
 const has_tooltip = document.getElementsByClassName('has-tooltip');
-var showingTooltip;
+
 for (let i = 0; i < has_tooltip.length; i++) {
-    has_tooltip[i].addEventListener('click', event => {
+    has_tooltip[i].addEventListener('click', (event) => {
         event.preventDefault();
         let tooltip = has_tooltip[i].getAttribute('title');
         let tooltipElem = document.createElement('div');
-        tooltipElem.className = "tooltip tooltip_active";
-        tooltipElem.innerHTML += '<div class="tooltip tooltip_active">' + tooltip + '</div>';
+        tooltipElem.classList.toggle = "tooltip_active";
+        tooltipElem.innerHTML += '<div class="tooltip" style=" left: 0; top: 0">' + tooltip + '</div>';
         has_tooltip[i].appendChild(tooltipElem); 
+        tooltipElem.classList.toggle = "tooltip_active";
+        //console.log(tooltipElem.className);
     })
+
+    
 }
