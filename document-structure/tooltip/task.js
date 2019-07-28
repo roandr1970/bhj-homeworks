8514ddc2362411539d0194f05a0d43f2b0eb836d
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-28 21:23:47 +0400

    Тема 6. Подсказки. Попытка смена класса при повторном щелчке

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,10 +5,10 @@ for (let i = 0; i < has_tooltip.length; i++) {
         event.preventDefault();
         let tooltip = has_tooltip[i].getAttribute('title');
         let tooltipElem = document.createElement('div');
-        tooltipElem.classList.toggle = "tooltip_active";
+        tooltipElem.classList.toggle("tooltip_active");
         tooltipElem.innerHTML += '<div class="tooltip" style=" left: 0; top: 0">' + tooltip + '</div>';
         has_tooltip[i].appendChild(tooltipElem); 
-        tooltipElem.classList.toggle = "tooltip_active";
+        tooltipElem.classList.toggle("tooltip_active");
         //console.log(tooltipElem.className);
     })
 
