b078d7e8f9b9179015d17be4a9263e51efdfb5c2
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-28 19:15:54 +0400

    Тема 6. Изменение структуры HTML-документа. Задание 2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,6 +1,7 @@
 const task_input = document.getElementById('task__input');
+const tasks_add = document.getElementById('tasks__add'); 
 
-task_input.addEventListener('change', (event) =>  {
+tasks_add.addEventListener('click', (event) =>  {
     event.preventDefault();
     const task_text = task_input.value;
     const tasks_list = document.getElementById('tasks__list');
@@ -13,11 +14,12 @@ task_input.addEventListener('change', (event) =>  {
         </div>`
     tasks_list.appendChild(inputElement);
     
-    const task_remove = document.getElementsByClassName('task__remove')[0];
-
-    task_remove.addEventListener('click', () => {
-        task_remove.remove();
-    })
+    const task_remove = document.getElementsByClassName('task__remove');
+    for (let i = 0; i < task_remove.length; i++) {
+        task_remove[i].addEventListener('click', () => {
+            inputElement.remove();
+        })
+    }
 })
 
 
