c63689399751fe20c58d55910e65d54dd9d9bd45
Andrey Rodionov <ambal007a@yandex.ru>
2019-07-31 18:18:47 +0400

    Тема6. Подсказки. Итоговый вариант

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,13 +1,19 @@
 const has_tooltip = document.getElementsByClassName('has-tooltip');
-const tooltip = document.getElementsByClassName('tooltip')[0];
+//const inputElement = document.getElementsByClassName('tooltip')[0];
+const inputElement = document.createElement('div');
 
 for (let i = 0; i < has_tooltip.length; i++) {
     has_tooltip[i].addEventListener('click', (event) => {
         event.preventDefault();
+        //const tooltip = document.getElementsByClassName('tooltip')[0];
         const tooltip_text = has_tooltip[i].getAttribute('title');
-        tooltip.classList.toggle("tooltip_active");
-        tooltip.style.left = has_tooltip[i].getBoundingClientRect().left + "px";
-        tooltip.style.top = (has_tooltip[i].getBoundingClientRect().top + 15) + "px";
-        tooltip.innerHTML = '<div class="tooltip tooltip_active">' + tooltip_text + '</div>';
+        //const inputElement = document.createElement('div');
+        inputElement.classList.add("tooltip");
+        inputElement.classList.toggle("tooltip_active");
+        inputElement.style.left = has_tooltip[i].getBoundingClientRect().left + "px";
+        inputElement.style.top = (has_tooltip[i].getBoundingClientRect().top + 15) + "px";
+        inputElement.innerHTML = '<div class="tooltip tooltip_active">' + tooltip_text + '</div>';
+        console.log(inputElement);
+        has_tooltip[i].appendChild(inputElement);
     })    
 }
